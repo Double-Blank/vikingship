3541837665d7fb9467f2989527f3ab75822b406c
Viking Zhang <vikingmute@gmail.com>
2019-12-06 11:03:45 +0800

    4-9 add button style, part 1

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,12 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <Button disabled> Hello </Button>
-        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Hello </Button>
-        <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled> Baidu Link </Button>
+        <Button> Hello </Button>
+        <Button disabled> Disabled Button </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com"> Baidu Link </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled> Disabled Link </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
