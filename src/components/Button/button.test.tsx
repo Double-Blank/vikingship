eec322eb34df96fe9f6248d8cbff71c3acfb5289
Viking Zhang <vikingmute@gmail.com>
2019-12-10 07:40:52 +0800

    5-4 add Button test file part 1

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -2,8 +2,22 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import Button from './button'
 
-test('our first react test case', () => {
-  const wrapper = render(<Button>Nice</Button>)
-  const element = wrapper.queryByText('Nice')
-  expect(element).toBeTruthy()
+
+describe('test Button component', () => {
+  it('should render the correct default button', () => {
+    const wrapper = render(<Button>Nice</Button>)
+    const element = wrapper.getByText('Nice')
+    expect(element).toBeInTheDocument()
+    expect(element.tagName).toEqual('BUTTON')
+    expect(element).toHaveClass('btn btn-default')
+  })
+  it('should render the correct component based on different props', () => {
+
+  })
+  it('should render a link when btnType equals link and href is provided', () => {
+
+  })
+  it('should render disabled button when disabled set to true', () => {
+
+  })
 })
