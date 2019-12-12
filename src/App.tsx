2a1e63d29777f2c80e95cf11cca30ebc79bf3cb9
Viking Zhang <vikingmute@gmail.com>
2019-12-12 14:49:58 +0800

    6-2 Menu component coding part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import Button, { ButtonType, ButtonSize } from './components/Button/button'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'  
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu defaultIndex={0} >
+          <MenuItem>
+            cool link
+          </MenuItem>
+          <MenuItem>
+            cool link 2
+          </MenuItem>        
+          <MenuItem>
+            cool link 3
+          </MenuItem>
+        </Menu>
         <Button className="custom"> Hello </Button>
         <Button disabled> Disabled Button </Button>
         <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
