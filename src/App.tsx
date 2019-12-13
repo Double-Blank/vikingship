10d3370edec26aa0b9b9589a5f137c162bea1d8f
Viking Zhang <vikingmute@gmail.com>
2019-12-13 17:44:00 +0800

    6-3 Menu component coding part 2

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,14 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <Menu defaultIndex={0} >
-          <MenuItem>
+        <Menu defaultIndex={0} onSelect={(index) => {alert(index)}} mode="vertical">
+          <MenuItem index={0}>
             cool link
           </MenuItem>
-          <MenuItem>
+          <MenuItem index={1} disabled>
             cool link 2
           </MenuItem>        
-          <MenuItem>
+          <MenuItem index={2}>
             cool link 3
           </MenuItem>
         </Menu>
