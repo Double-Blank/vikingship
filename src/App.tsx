f64758af179cbac04edaaddf863c6eb982ec8bf7
Viking Zhang <vikingmute@gmail.com>
2019-12-16 16:00:53 +0800

    6-7 SubMenu component coding part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
 import React from 'react';
-import Button, { ButtonType, ButtonSize } from './components/Button/button'
 import Menu from './components/Menu/menu'
 import MenuItem from './components/Menu/menuItem'  
-
+import SubMenu from './components/Menu/subMenu'
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <Menu defaultIndex={0} onSelect={(index) => {alert(index)}} mode="vertical">
-          <MenuItem index={0}>
+        <Menu defaultIndex={0} onSelect={(index) => {alert(index)}}>
+          <MenuItem>
             cool link
           </MenuItem>
-          <MenuItem index={1} disabled>
+          <MenuItem disabled>
             cool link 2
-          </MenuItem>        
-          <MenuItem index={2}>
+          </MenuItem> 
+          <SubMenu title="dropdown">
+            <MenuItem>
+              dropdown 1
+            </MenuItem>
+            <MenuItem>
+              dropdown 2
+            </MenuItem>
+          </SubMenu>        
+          <MenuItem>
             cool link 3
           </MenuItem>
         </Menu>
-        <Button className="custom"> Hello </Button>
-        <Button disabled> Disabled Button </Button>
-        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
-        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
-        <Button btnType={ButtonType.Link} href="http://www.baidu.com" target="_blank"> Baidu Link </Button>
-        <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled> Disabled Link </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
