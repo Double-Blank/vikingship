50383fbaa96ad03de2703cdf2b0776619cf5d535
Viking Zhang <vikingmute@gmail.com>
2019-12-19 16:39:52 +0800

    7-2 Icon component coding part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import Menu from './components/Menu/menu'
 import MenuItem from './components/Menu/menuItem'  
 import SubMenu from './components/Menu/subMenu'
+import Icon from './components/Icon/icon'
+library.add(fas)
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <Icon icon="arrow-down" theme="danger" size="10x" />
         <Menu defaultIndex='0' onSelect={(index) => {alert(index)}} mode="vertical" defaultOpenSubMenus={['2']}>
           <MenuItem>
             cool link
