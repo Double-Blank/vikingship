ecf768e048d9790c0ab033f52ece6293a44b0c5c
Viking Zhang <vikingmute@gmail.com>
2019-12-20 10:27:14 +0800

    7-3 Icon component add styles use sass @each helper

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <Icon icon="arrow-down" theme="danger" size="10x" />
+        <Icon icon="arrow-down" theme="primary" size="10x" />
         <Menu defaultIndex='0' onSelect={(index) => {alert(index)}} mode="vertical" defaultOpenSubMenus={['2']}>
           <MenuItem>
             cool link
