5a96b37988ea1d55bf0626b8b9298d905af764a3
Viking Zhang <vikingmute@gmail.com>
2019-12-23 21:19:30 +0800

    7-6 coding with CSSTransitionGroup part 2

--- src/components/Menu/subMenu.tsx
+++ src/components/Menu/subMenu.tsx
@@ -59,6 +59,7 @@ const SubMenu: React.FC<SubMenuProps> = ({ index, title, children, className}) =
         timeout={300}
         classNames="zoom-in-top"
         appear
+        unmountOnExit
       >
         <ul className={subMenuClasses}>
           {childrenComponent}
