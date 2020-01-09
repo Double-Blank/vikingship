c665529f70c58adcfc789e19c3afcc48c014b864
Viking Zhang <vikingmute@gmail.com>
2020-01-09 16:05:01 +0800

    9-7 AutoComplete component coding part 3 - support async request

--- src/components/AutoComplete/autoComplete.stories.tsx
+++ src/components/AutoComplete/autoComplete.stories.tsx
@@ -6,6 +6,11 @@ interface LakerPlayerProps {
   value: string;
   number: number;
 }
+interface GithubUserProps {
+  login: string;
+  url: string;
+  avatar_url: string;
+}
 const SimpleComplete = () => {
   const lakers = ['bradley', 'pope', 'caruso', 'cook', 'cousins',
   'james', 'AD', 'green', 'howard', 'kuzma', 'McGee', 'rando']
@@ -24,14 +29,23 @@ const SimpleComplete = () => {
   // const handleFetch = (query: string) => {
   //   return lakers.filter(name => name.includes(query)).map(name => ({value: name}))
   // }
+  // const handleFetch = (query: string) => {
+  //   return lakersWithNumber.filter(player => player.value.includes(query))
+  // }
   const handleFetch = (query: string) => {
-    return lakersWithNumber.filter(player => player.value.includes(query))
+    return fetch(`https://api.github.com/search/users?q=${query}`)
+      .then(res => res.json())
+      .then(({ items }) => {
+        console.log(items)
+        return items.slice(0, 10).map(item => ({ value: item.login, ...item}))
+      })
   }
-  const renderOption = (item: DataSourceType<LakerPlayerProps>) => {
+
+  const renderOption = (item: DataSourceType<GithubUserProps>) => {
     return (
       <>
-        <h2>Name: {item.value}</h2>
-        <p>Number: {item.number}</p>
+        <h2>Name: {item.login}</h2>
+        <p>url: {item.url}</p>
       </>
     )
   }
--- src/components/AutoComplete/autoComplete.tsx
+++ src/components/AutoComplete/autoComplete.tsx
@@ -1,12 +1,12 @@
 import React, { FC, useState, ChangeEvent, ReactElement } from 'react'
 import Input, { InputProps } from '../Input/input'
-
+import Icon from '../Icon/icon'
 interface DataSourceObject {
   value: string;
 }
 export type DataSourceType<T = {}> = T & DataSourceObject
 export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
-  fetchSuggestions: (str: string) => DataSourceType[];
+  fetchSuggestions: (str: string) => DataSourceType[] | Promise<DataSourceType[]>;
   onSelect?: (item: DataSourceType) => void;
   renderOption?: (item: DataSourceType) => ReactElement;
 }
@@ -22,13 +22,23 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
 
   const [ inputValue, setInputValue ] = useState(value)
   const [ suggestions, setSugestions ] = useState<DataSourceType[]>([])
+  const [ loading, setLoading ] = useState(false)
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value.trim()
     setInputValue(value)
     if (value) {
       const results = fetchSuggestions(value)
-      setSugestions(results)
+      if (results instanceof Promise) {
+        console.log('triggered')
+        setLoading(true)
+        results.then(data => {
+          setLoading(false)
+          setSugestions(data)
+        })
+      } else {
+        setSugestions(results)
+      }
     } else {
       setSugestions([])
     }
@@ -63,6 +73,7 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
         onChange={handleChange}
         {...restProps}
       />
+      { loading && <ul><Icon icon="spinner" spin /></ul>}
       { (suggestions.length > 0) && generateDropdown()}
     </div>
   )
