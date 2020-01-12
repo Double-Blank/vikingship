fd772afc5cf736f6874bfd15212833f753b6287a
Viking Zhang <vikingmute@gmail.com>
2020-01-12 09:22:46 +0800

    9-10 AutoComplete component coding part 6 learn how to use useRef

--- src/components/AutoComplete/autoComplete.tsx
+++ src/components/AutoComplete/autoComplete.tsx
@@ -1,8 +1,9 @@
-import React, { FC, useState, ChangeEvent, KeyboardEvent, ReactElement, useEffect } from 'react'
+import React, { FC, useState, ChangeEvent, KeyboardEvent, ReactElement, useEffect, useRef } from 'react'
 import classNames from 'classnames'
 import Input, { InputProps } from '../Input/input'
 import Icon from '../Icon/icon'
 import useDebounce from '../../hooks/useDebounce'
+import useClickOutside from '../../hooks/useClickOutside'
 interface DataSourceObject {
   value: string;
 }
@@ -26,9 +27,12 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
   const [ suggestions, setSugestions ] = useState<DataSourceType[]>([])
   const [ loading, setLoading ] = useState(false)
   const [ highlightIndex, setHighlightIndex] = useState(-1)
+  const triggerSearch = useRef(false)
+  const componentRef = useRef<HTMLDivElement>(null)
   const debouncedValue = useDebounce(inputValue, 500)
+  useClickOutside(componentRef, () => { setSugestions([])})
   useEffect(() => {
-    if (debouncedValue) {
+    if (debouncedValue && triggerSearch.current) {
       const results = fetchSuggestions(debouncedValue)
       if (results instanceof Promise) {
         console.log('triggered')
@@ -75,6 +79,7 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value.trim()
     setInputValue(value)
+    triggerSearch.current = true
   }
   const handleSelect = (item: DataSourceType) => {
     setInputValue(item.value)
@@ -82,6 +87,7 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
     if (onSelect) {
       onSelect(item)
     }
+    triggerSearch.current = false
   }
   const renderTemplate = (item: DataSourceType) => {
     return renderOption ? renderOption(item) : item.value
@@ -103,7 +109,7 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
     )
   }
   return (
-    <div className="viking-auto-complete">
+    <div className="viking-auto-complete" ref={componentRef}>
       <Input
         value={inputValue}
         onChange={handleChange}
