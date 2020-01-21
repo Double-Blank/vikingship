52f08815ed3bfef22ff8dea3bb3b2b47d6f6d231
Viking Zhang <vikingmute@gmail.com>
2020-01-21 15:21:33 +0800

    10-12 add DrogAndDrop for upload component

--- src/components/Upload/upload.stories.tsx
+++ src/components/Upload/upload.stories.tsx
@@ -2,6 +2,8 @@ import React  from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { Upload, UploadFile } from './upload'
+import Button from '../Button/button'
+import Icon from '../Icon/icon'
 const defaultFileList: UploadFile[] = [
   { uid: '123', size: 1234, name: 'hello.md', status: 'uploading', percent: 30 },
   { uid: '122', size: 1234, name: 'xyz.md', status: 'success', percent: 30 },
@@ -25,11 +27,13 @@ const SimpleUpload = () => {
       onChange={action('changed')}
       onRemove={action('removed')}
       name="fileName"
-      data={{'key': 'value'}}
-      headers={{'X-Powered-By': 'vikingship'}}
-      accept=".jpg"
       multiple
-    />
+      drag
+    >
+      <Icon icon="upload" size="5x" theme="secondary" />
+      <br/>
+      <p>Drag file over to upload</p>
+    </Upload>
   )
 }
 
