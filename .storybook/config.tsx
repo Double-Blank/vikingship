d1fd30b4013cc8f1fb92fc5029b9763ebfbdc559
Viking Zhang <vikingmute@gmail.com>
2020-02-03 11:10:53 +0800

    12-5 update storybook index page

--- .storybook/config.tsx
+++ .storybook/config.tsx
@@ -18,5 +18,13 @@ const storyWrapper = (stroyFn: any) => (
 addDecorator(storyWrapper)
 addDecorator(withInfo)
 addParameters({info: { inline: true, header: false}})
+const loaderFn = () => {
+  const allExports = [require('../src/welcome.stories.tsx')];
+  const req = require.context('../src/components', true, /\.stories\.tsx$/);
+  req.keys().forEach(fname => allExports.push(req(fname)));
+  return allExports;
+};
+
+
 // automatically import all files ending in *.stories.js
-configure(require.context('../src', true, /\.stories\.tsx$/), module);
+configure(loaderFn, module);
