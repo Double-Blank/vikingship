a0401c4c25078325e39c7eec4f22371335f04cf3
Viking Zhang <vikingmute@gmail.com>
2022-06-12 10:03:09 +0800

    更新 Form 表单组件的测试以及 stories

--- src/components/Form/form.stories.tsx
+++ src/components/Form/form.stories.tsx
@@ -4,10 +4,11 @@ import Form, { IFormRef } from './form'
 import Item from './formItem'
 import Input from '../Input'
 import Button from '../Button'
+import Select from '../Select'
 import { CustomRule } from './useStore'
 
 const meta: ComponentMeta<typeof Form> ={ 
-  title: 'Form 组件',
+  title: '第十一章:Form 组件',
   id: 'Form',
   component: Form,
   subcomponents: { 'Item': Item },
@@ -18,6 +19,13 @@ const meta: ComponentMeta<typeof Form> ={
       </div>
     ),
   ],
+  parameters: {
+    docs: {
+      source: {
+        type: "code",
+      },
+    }
+  }
 }
 export default meta
 const confirmRules: CustomRule[] = [
@@ -38,7 +46,68 @@ const confirmRules: CustomRule[] = [
     }
   })
 ]
-export const BasicForm = (args) => {
+export const ABasicForm = (args) => {
+  return (
+    <Form {...args} >
+      <Item label='用户名' name='name' rules={[{type: 'string',required: true, min: 3}]}>
+        <Input/>
+      </Item>
+      <Item label='密码' name='password' rules={[{type: 'string',required: true, min: 3, max: 8 }]}>
+        <Input type="password"/>
+      </Item>
+      <div className='viking-form-submit-area'>
+        <Button type="submit" btnType='primary'>登陆</Button>
+      </div>
+    </Form>
+  )
+}
+ABasicForm.storyName = '基本的登陆表单'
+
+export const BRegForm = (args) => { 
+  const initialValues = {
+    agreement: false
+  }
+  return (
+    <Form {...args} initialValues={initialValues}>
+      <Item label='邮件' name='email' rules={[{ type: 'email',required: true }]}>
+        <Input/>
+      </Item>
+      <Item label='密码' name='password' rules={[{type: 'string',required: true, min: 3, max: 8 }]}>
+        <Input type="password"/>
+      </Item>
+      <Item 
+        label='性别' 
+        name='gender'
+        rules={[{type: 'string',required: true }]}
+        getValueFromEvent={(e) => e }
+        valuePropName='defaultValue'
+      >
+        <Select
+          placeholder="请选择性别"
+        >
+          <Select.Option value="男" />
+          <Select.Option value="女" />
+        </Select>
+      </Item>
+      <div className='agreement-section' style={{ 'display': 'flex', 'justifyContent': 'center'}}>
+        <Item
+          name='agreement' 
+          rules={[{ type: 'enum', enum: [true], message: '请同意协议'}]}
+          getValueFromEvent={(e) => e.target.checked }
+          valuePropName='checked'
+        >
+          <input type="checkbox"/>
+        </Item>
+        <span className="agree-text">注册即代表你同意<a href='#'>用户协议</a></span>
+      </div>
+      <div className='viking-form-submit-area'>
+        <Button type="submit" btnType='primary'>登陆</Button>
+      </div>
+    </Form>
+  )
+}
+BRegForm.storyName = '注册表单，支持多种 FormItem 组件'
+export const CFullForm = (args) => {
   const ref = useRef<IFormRef>()
   const resetAll = () => {
     console.log('form ref', ref.current)
@@ -79,3 +148,5 @@ export const BasicForm = (args) => {
     </Form>
   )
 }
+
+CFullForm.storyName = '自定义规则，调用表单实例'
--- src/components/Form/form.tsx
+++ src/components/Form/form.tsx
@@ -3,10 +3,14 @@ import { ValidateError } from 'async-validator'
 import useStore, { FormState }from './useStore';
 export type RenderProps = (form: FormState) => ReactNode
 export interface FormProps {
+  /**表单名称，会作为表单字段 id 前缀使用 */
   name?: string;
+  /**表单默认值，只有初始化以及重置时生效 */
   initialValues?: Record<string, any>;
   children?: ReactNode | RenderProps;
+  /**提交表单且数据验证成功后回调事件 */
   onFinish?: (values: Record<string, any>) => void;
+  /**提交表单且数据验证失败后回调事件 */
   onFinishFailed?: (values: Record<string, any>, errors: Record<string, ValidateError[]>) => void;
 }
 export type IFormContext = 
@@ -46,18 +50,11 @@ export const Form = forwardRef<IFormRef, FormProps>((props, ref) => {
     childrenNode = children
   }
   return (
-    <>
-      <form name={name} className="viking-form" onSubmit={submitForm}>
-        <FormContext.Provider value={passedContext}>
-          {childrenNode}
-        </FormContext.Provider>
-      </form>
-      <div>
-        <pre style={{whiteSpace: 'pre-wrap'}}>{JSON.stringify(fields)}</pre>
-        <pre style={{whiteSpace: 'pre-wrap'}}>{JSON.stringify(form)}</pre>
-      </div>
-    </>
-
+    <form name={name} className="viking-form" onSubmit={submitForm}>
+      <FormContext.Provider value={passedContext}>
+        {childrenNode}
+      </FormContext.Provider>
+    </form>
   )
 })
 Form.defaultProps = {
