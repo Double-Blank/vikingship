7ee5b09b7807a98c5e24870be4936df5313debff
Double-Blank <a13633205983@163.com>
2022-11-19 15:16:07 +0800

    笔记

--- src/components/Button/button.stories.tsx
+++ src/components/Button/button.stories.tsx
@@ -7,11 +7,6 @@ import Button from './button'
 export default {
   title: '第四章：Button',
   component: Button,
-  // parameters: {
-  //   docs: {
-  //     page: WelcomeMDX
-  //   }
-  // }
 } as ComponentMeta<typeof Button>
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
@@ -21,33 +16,6 @@ ADefault.args = {
   children: 'Default Button',
 }
 ADefault.storyName = '默认按钮样式'
-// export const Large = Template.bind({})
-// Large.args = {
-//   size: 'lg',
-//   children: 'Large Button',
-// }
-// export const Small = Template.bind({})
-// Small.args = {
-//   size: 'sm',
-//   children: 'Small Button',
-// }
-// export const Primary = Template.bind({})
-// Primary.args = {
-//   btnType: 'primary',
-//   children: 'Primary Button',
-// }
-// export const Danger = Template.bind({})
-// Danger.args = {
-//   btnType: 'danger',
-//   children: 'Danger Button',
-// }
-// export const Link = Template.bind({})
-// Link.args = {
-//   btnType: 'link',
-//   children: 'Link Button',
-//   href: 'https://google.com'
-// }
-
 export const BButtonWithSize = () => (
   <>
     <Button size="lg"> large button </Button>
